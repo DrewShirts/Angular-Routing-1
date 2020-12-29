fcd63eadf66c9eefa54016db5e96241a8b7cf2ed
Drew Shirts <drewshirts@gmail.com>
2020-12-29 10:45:42 -0700

    Reacting to Routing Events

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 import { AuthService } from './user/auth.service';
 import { slideInAnimation } from './app.animation';
@@ -12,8 +12,25 @@ import { slideInAnimation } from './app.animation';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  loading = true;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd
+        || routerEvent instanceof NavigationCancel
+        || routerEvent instanceof NavigationError) {
+          this.loading = false;
+    }
+  }
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
