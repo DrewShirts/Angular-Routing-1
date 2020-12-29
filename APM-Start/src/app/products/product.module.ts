1d775d44be4e9325e63cb6b3334eb5b801f3a186
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-28 20:28:11 -0700

    Adding a Resolver to a Route Configuration

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductResolver } from './product-resolver.service';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -12,8 +13,16 @@ import { SharedModule } from '../shared/shared.module';
     SharedModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent }
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver }
+      },
+      {
+        path: 'products/:id/edit',
+        component: ProductEditComponent,
+        resolve: { resolvedData: ProductResolver }
+      }
     ])
   ],
   declarations: [
