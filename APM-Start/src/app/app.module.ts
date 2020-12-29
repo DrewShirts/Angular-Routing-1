7340de11f20012a5d705aed608650d60c635082a
Drew Shirts <drewshirts@gmail.com>
2020-12-29 13:46:08 -0700

    Preparing for Lazy Loading

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 
@@ -23,7 +22,6 @@ import { MessageModule } from './messages/message.module';
     BrowserAnimationsModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    ProductModule,
     UserModule,
     MessageModule,
     AppRoutingModule
