3633caf53bb29bb3224b6c91f7530eb4a5f328d7
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-28 20:14:07 -0700

    Building a Route Resolver Service

---
+++ APM-Start/src/app/products/product-resolver.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductResolver implements Resolve<Product> {
+
+  constructor(private productService: ProductService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+    const id = +route.paramMap.get('id');
+
+    return null;
+  }
+
+}
