0be78c3155dba7d5b0389e7fcd051e690988e64c
Drew Shirts <drewshirts@gmail.com>
2020-12-28 22:41:30 -0700

    Configuring Child Routes

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
 import { ProductResolver } from './product-resolver.service';
 
 import { SharedModule } from '../shared/shared.module';
@@ -21,14 +23,31 @@ import { SharedModule } from '../shared/shared.module';
       {
         path: 'products/:id/edit',
         component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver }
+        resolve: { resolvedData: ProductResolver },
+        children: [
+          {
+            path: '',
+            redirectTo: 'info',
+            pathMatch: 'full'
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
       }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
